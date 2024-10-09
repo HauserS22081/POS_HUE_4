870bd3b090f1e8a1a9a638c3568e9ad8a4fc2681
HauserS22081 <HauserS22081@sus.htl-grieskirchen.at>
2024-10-09 10:27:14 +0200

    Android Studio UI

--- HUE4_1Assignment/app/src/main/java/at/htlgkr/pos/DashboardFragment.java
+++ HUE4_1Assignment/app/src/main/java/at/htlgkr/pos/DashboardFragment.java
@@ -1,9 +1,29 @@
 package at.htlgkr.pos;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
 import androidx.fragment.app.Fragment;
+
+import at.htlgkr.pos.databinding.FragmentDashboardBinding;
 
 public class DashboardFragment extends Fragment {
 
+    private FragmentDashboardBinding binding;
+
     public DashboardFragment() {
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        binding = FragmentDashboardBinding.inflate(inflater, container, false);
+        return binding.getRoot();
     }
 }
